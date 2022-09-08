183b5530d3605a74a20ff25f2c058b83be4649d1
jamesfromonline <jtwebco@gmail.com>
2022-09-08 16:42:11 -0400

    tweaks on work items

--- src/workItems.ts
+++ src/workItems.ts
@@ -9,14 +9,17 @@ export const workItems = [
     logo: `${url}/littlebuilds-logo-icon.svg`,
     image: "",
     description:
-      "little builds offers a simple, secure, and intuitive way to buy and sell verified digital assets like web sites, mobile apps, and domains. for transactions ranging from $10 to $100,000, little builds offers an escrow service (via <strong>escrow.com</strong api) for the most secure experience for transferring web asset ownership. Stripe and Paypal are also offered for transactions up to $10,000. <br /><br /><br /> the platform is being built from the ground up with the ultimate user experience being kept in mind at every step of the way. i am currently on track to launch little builds in late 2022!",
+      "little builds offers a simple, secure, and intuitive way to buy and sell verified digital assets like web sites, mobile apps, and domains. for transactions ranging from $10 to $100,000, little builds offers an escrow service (via <strong>escrow.com</strong api) for the most secure experience for transferring web asset ownership. stripe and paypal are also offered for transactions up to $10,000. <br /><br /><br /> the platform is being built from the ground up with the ultimate user experience being kept in mind at every step of the way. i am currently hoping to launch little builds in q2 of 2023!",
     url: "https://littlebuilds.io",
     stack: [
       "typescript",
       "react",
       "nextjs",
+      "prisma",
+      "postgresql",
+      "trpc",
+      "nextauth",
       "websockets",
-      "mongodb",
       "tailwindcss"
     ]
   },
@@ -31,7 +34,7 @@ export const workItems = [
       "nextjs",
       "react",
       "node",
-      "twitter",
+      "twitter api",
       "mongodb",
       "tailwindcss",
       "progressive web app"
@@ -60,7 +63,7 @@ export const workItems = [
     logo: `${url}/mepage-kevin-icon.svg`,
     image: "",
     description:
-      "mepage is a link-in-bio webapp that allows users to create highly customized pages with several different types of links that offer unique functionality. users can easily create a personalized landing page and organize digital life under one umbrella.<br /><br />this was my first foray into building an entire fullstack SaaS business from scratch to acquirement. i was able to learn a TON about the SaaS development process, and how to effectively communicate with clients and users to both solve problems and enhance the overall experience.",
+      "mepage (acquired in q2 of 2022) is a link-in-bio webapp that allows users to create highly customized pages with several different types of links that offer unique functionality. users can easily create a personalized landing page and organize digital life under one umbrella.<br /><br />this was my first foray into building an entire full-stack saas business from scratch to acquirement. i was able to learn a ton about the saas development process, and how to effectively communicate with users to both solve problems and enhance the overall user experience.<br /><br />mepage was acquired in q2 of 2022 by a private buyer and i no longer maintain it.",
     url: "https://mepage.is",
     stack: [
       "react",
